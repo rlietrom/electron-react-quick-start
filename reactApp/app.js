6f873e1a85f053f11eda0193e2c42b9c0f1f7a46
Carlie Ostrom <rlietrom@gmail.com>
2017-07-24 11:54:55 -0700

    componentfiles

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var EditorView = require('./components/editorView');
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
+ReactDOM.render(<EditorView/>,
    document.getElementById('root'));
---
+++ reactApp/components/editorView.js
@@ -0,0 +1,27 @@
+var React = require('react');
+var Toolbox = require('./toolbox');
+var ReactDOM = require('react-dom');
+var {Editor, EditorState} = require('draft-js');
+
+class EditorView extends React.Component {
+  constructor(props) {
+    super(props)
+    // this.state = {
+    //   editorState: EditorState.createEmpty()
+    // }
+    // this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  render() {
+    return (
+      <div>
+        <p>HEYYY THIS IS EDITOR VIEW</p>
+        <Toolbox/>
+      </div>
+    )
+  }
+}
+
+
+
+module.exports = EditorView;
