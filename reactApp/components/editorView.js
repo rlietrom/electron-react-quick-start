6edaa1ce5d2397afccfadc4e04c0f657743e6f51
Carlie Ostrom <rlietrom@gmail.com>
2017-07-24 15:00:51 -0700

    b

--- reactApp/components/editorView.js
+++ reactApp/components/editorView.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var Toolbox = require('./toolbox');
 var ReactDOM = require('react-dom');
-var {RichUtils, Editor, EditorState} = require('draft-js');
+var {Editor, EditorState, RichUtils} = require('draft-js');
 //import styles from '../styles.css';
 
 class EditorView extends React.Component {
@@ -11,33 +11,49 @@ class EditorView extends React.Component {
       editorState: EditorState.createEmpty()
     }
     this.onChange = (editorState) => this.setState({editorState});
-
   }
 
-   clickHandler(btn) {
-    if(btn === "BOLD"){
+  const styleMap = {
+    'STRIKETHROUGH': {
+      textDecoration: 'line-through',
+    }
+  };
+
+  clickHandler(btn) {
+    if(btn === 'BOLD'){
       this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    }
+    if(btn === 'ITALIC'){
+      this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+    }
+    if(btn === 'UNDERLINE'){
+      this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+    }
+    if(btn === 'CODE'){
+      this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'CODE'));
+    }
+    if(btn === 'STRIKETHROUGH'){
+      this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'STRIKETHROUGH'));
     }
   }
 
-
-  // _onBoldClick() {
-  //   this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  // }
 
 
   render() {
     return (
       <div>
         <p>HEYYY THIS IS EDITOR VIEW</p>
-        {/* <button onClick={this._onBoldClick.bind(this)}>Bold</button> */}
-        <Editor editorState={this.state.editorState} onChange={this.onChange}/>
+        <div className="box">
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          customStyleMap={styleMap}
+        />
+        </div>
         <Toolbox clickHandler={(btn) => this.clickHandler(btn)}/>
       </div>
     )
   }
 }
-
-
 
 module.exports = EditorView;
