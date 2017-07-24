a894628d79ed20d094e409c68725cd50b79b5458
Carlie Ostrom <rlietrom@gmail.com>
2017-07-24 12:40:14 -0700

    editview

--- reactApp/components/editorView.js
+++ reactApp/components/editorView.js
@@ -6,16 +6,17 @@ var {Editor, EditorState} = require('draft-js');
 class EditorView extends React.Component {
   constructor(props) {
     super(props)
-    // this.state = {
-    //   editorState: EditorState.createEmpty()
-    // }
-    // this.onChange = (editorState) => this.setState({editorState});
+    this.state = {
+      editorState: EditorState.createEmpty()
+    }
+    this.onChange = (editorState) => this.setState({editorState});
   }
 
   render() {
     return (
       <div>
         <p>HEYYY THIS IS EDITOR VIEW</p>
+        <Editor editorState={this.state.editorState} onChange={this.onChange}/>
         <Toolbox/>
       </div>
     )
