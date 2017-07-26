6006ac9cb5a07d84d49e7acafdfb8fe4aa424c8f
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-07-26 10:09:45 -0700

    small changes

--- reactApp/components/editorView.js
+++ reactApp/components/editorView.js
@@ -166,39 +166,8 @@ class EditorView extends React.Component {
           {this.increaseFontSize(false)}
           {this.increaseFontSize(true)}
         </div>
-
-//   clickHandler(btn) {
-//     if(btn === 'BOLD'){
-//       this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-//     }
-//     if(btn === 'ITALIC'){
-//       this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
-//     }
-//     if(btn === 'UNDERLINE'){
-//       this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-//     }
-//     if(btn === 'CODE'){
-//       this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'CODE'));
-//     }
-//     if(btn === 'STRIKETHROUGH'){
-//       this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'STRIKETHROUGH'));
-//     }
-//     if(btn === 'PINK'){
-//       this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'PINK'));
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         {/* <AppBar title={"Team Curl"}/> */}
-//         <h1>sampleDocumnent(unimplemented)</h1>
-//         <p>Shareable Document ID: 123456789(unimplemented)</p>
-//         <button>Save Changes (unimplemented)</button>
-//         {/* <button onClick={this._onBoldClick.bind(this)}>Bold</button> */}
         <div className='editor'>
         <Editor
-
           ref="editor"
           customStyleMap={this.state.inlineStyles}
           editorState={this.state.editorState}
@@ -207,7 +176,6 @@ class EditorView extends React.Component {
         />
         </div>
       </div>
-
     )
   }
 }
